61b8d1bc052173c0df0e7cbb89a50ec524ee8d4d
Abdullah Alsigar <abdullahi.iraq@gmail.com>
2020-07-21 21:56:19 +0300

    useNativeDriver: false

--- src/index.js
+++ src/index.js
@@ -146,7 +146,7 @@ export default class Swipeable extends PureComponent {
       toValue: {x: 0, y: 0},
       duration: 250,
       easing: Easing.elastic(0.5),
-      useNativeDriver: true 
+      useNativeDriver: false 
     },
 
     // misc
@@ -205,7 +205,7 @@ export default class Swipeable extends PureComponent {
   _handlePan = Animated.event([null, {
     dx: this.state.pan.x,
     dy: this.state.pan.y
-  }]);
+  }], { useNativeDriver: false });
 
   _handleMoveShouldSetPanResponder = (event, gestureState) => (
     Math.abs(gestureState.dx) > this.props.swipeStartMinDistance
